7540096c6d6e7b5aa72d98ebdb2676e4e5ab302b
Ron Javellana <Ronapj1991@gmail.com>
2023-01-16 20:23:42 -0500

    add peaktimes logic

--- lib/peaktimes.rb
+++ lib/peaktimes.rb
@@ -1,16 +1,22 @@
-require 'date'
-#https://www.rubyguides.com/2015/12/ruby-time/
-#https://ruby-doc.org/core-3.1.1/Time.html#method-i-hour
+require 'csv'
+require 'time'
 
-#https://github.com/TheOdinProject/curriculum/blob/main/ruby/files_and_serialization/event_attendees.csv
-#Open the CSV file
-#iterate over each row
-#Grab the RegDate
-#Need to extract hours from the dates
-#Examples: "11/12/08 10:47" , 2/2/09 11:29
-  #"11/12/08 10:47".split(" ")
-  #["11/12/08", "10:47"] so the index 1 gives the time
-  #"11/12/08 10:47".split(" ")[1].split(":")[0] gives the hours per row
-  #Create a hash Hash.new(0) initial value 0
-  #if the hour does not exist in the created hash, add it to the hash and increment the value
-  #iterate through the has and output the count per hour
+def extract_hour(str_date)
+  Time.strptime(str_date, "%M/%d/%Y %H:%M").strftime("%l%p")
+end
+
+contents = CSV.open("event_attendees.csv",
+  headers: true,
+  header_converters: :symbol
+)
+
+hour_count = Hash.new(0)
+
+contents.each do |row|
+  hour = extract_hour(row[:regdate])
+  hour_count[hour] += 1
+end
+
+hour_count.each do |key, val|
+  puts "At #{key}, there were #{val} registrations"
+end
